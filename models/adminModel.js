ac837fcefc409efd61ec4f5d955c53bf2d08b9b6
pratheeshraj <rajeshnsk5@gmail.com>
2024-07-27 13:04:59 +0530

    add

---
+++ models/adminModel.js
@@ -0,0 +1,20 @@
+const mongoose=require("mongoose");
+
+
+const AdminSchema = new mongoose.Schema({
+  email: {
+    required: [true, "please enter your email address"],
+    type: String,
+  },
+  pasworrd: {
+    required: [true, "please enter a password"],
+    type: String,
+  },
+  role: {
+    type: String,
+    default: "admin",
+  },
+});
+
+const adminModel=mongoose.model("Admin",AdminSchema)
+module.exports = adminModel
--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const DbConnection = require("./config/dbConnection");
 dotenv.config({ path: path.join(__dirname, "config/config.env") });
 const PORT = process.env.PORT || 4000;
 const userRouter=require("./router/userRouter")
+const adminRouter=require("./router/AdminRouter")
 // middleware
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api/user",userRouter)
+app.use("/api/admin",adminRouter)
 // db connected
 DbConnection();
 app.listen(PORT, () => {
