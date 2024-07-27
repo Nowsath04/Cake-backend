b18edbfd65eedc30aaf979c753d4861b0a2e780a
Nowsath04 <nowsathnichu@gmail.com>
2024-07-27 13:04:24 +0530

    added

--- models/EmailVerificationModel.js
+++ models/EmailVerificationModel.js
@@ -1,24 +1,25 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const EmailVerificationSchema=new mongoose.Schema({
-    userid:{
-        type:String,
-        required:true
+const EmailVerificationSchema = new mongoose.Schema({
+    userid: {
+        type: String,
+        required: true
+
     },
-    OTP:{
-        type:String,
-        required:true
+    OTP: {
+        type: String,
+        required: true
     },
-    createdat:{
-        type:Date,
-        required:true
+    createdat: {
+        type: Date,
+        required: true
     },
-    expiresAt:{
-        type:Date,
-        required:true
+    expiresAt: {
+        type: Date,
+        required: true
     }
-   
+
 })
 
-const EmailVerificationModel=mongoose.model("EmailVerificationModel",EmailVerificationSchema)
+const EmailVerificationModel = mongoose.model("EmailVerificationModel", EmailVerificationSchema)
 module.exports = EmailVerificationModel
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,10 +1,12 @@
-const express=require("express")
-const { RegisterUser, VerifyEmail, LoginUser } = require("../controller/userController")
+const express = require("express")
+const { RegisterUser, VerifyEmail, LoginUser, CreateNonce } = require("../controller/userController")
 
-const router=express.Router()
+const router = express.Router()
 
-router.post("/register",RegisterUser)
-router.post("/verify-email",VerifyEmail)
-router.post("/login",LoginUser)
+router.post("/generate-nonce", CreateNonce)
+// router.post("/verify-login", CheckUser)
+router.post("/register", RegisterUser)
+router.post("/verify-email", VerifyEmail)
+router.post("/login", LoginUser)
 
-module.exports=router
+module.exports = router
