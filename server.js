1480795f8e0ebd103b169ff5d19d52783f298187
Nowsath04 <nowsathnichu@gmail.com>
2024-07-27 12:23:10 +0530

    added

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ const path = require("path");
 const DbConnection = require("./config/dbConnection");
 dotenv.config({ path: path.join(__dirname, "config/config.env") });
 const PORT = process.env.PORT || 4000;
-const userRouter=require("./router/userRouter")
+const userRouter = require("./router/userRouter")
 // middleware
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use("/api/user",userRouter)
+app.use("/api/user", userRouter)
 // db connected
 DbConnection();
 app.listen(PORT, () => {
   console.log(`listening on ${PORT} on ${process.env.NODE_ENV}`);
 });
+
